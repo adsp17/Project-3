57dbd6b237a0aec9e4eed4753d83ca3ee9616cc5
Muhammad Adhisurya Putra <141158683+adsp17@users.noreply.github.com>
2025-07-15 23:18:34 +0700

    Create ControlVolume.ino

---
+++ ControlVolume/ControlVolume.ino
@@ -0,0 +1,46 @@
+#include <HID-Project.h>  // Library HID Project: https://github.com/NicoHood/HID
+#include <HID-Settings.h>
+
+const int pinCLK = 2;  // Sinyal A
+const int pinDT  = 3;  // Sinyal B
+const int pinSW  = 4;  // Tombol tekan (opsional)
+
+int lastCLKState;
+bool isMuted = false;
+
+void setup() {
+  pinMode(pinCLK, INPUT);
+  pinMode(pinDT, INPUT);
+  pinMode(pinSW, INPUT_PULLUP);  // Tombol biasanya aktif LOW
+
+  lastCLKState = digitalRead(pinCLK);
+
+  // Mulai komunikasi HID
+  Consumer.begin();
+}
+
+void loop() {
+  int currentCLKState = digitalRead(pinCLK);
+
+  // Deteksi perubahan putaran
+  if (currentCLKState != lastCLKState && currentCLKState == LOW) {
+    int dtState = digitalRead(pinDT);
+    if (dtState != currentCLKState) {
+      // Putaran ke kanan - volume up
+      Consumer.write(MEDIA_VOLUME_UP);
+    } else {
+      // Putaran ke kiri - volume down
+      Consumer.write(MEDIA_VOLUME_DOWN);
+    }
+  }
+  lastCLKState = currentCLKState;
+
+  // Tombol tekan encoder untuk mute/unmute
+  if (digitalRead(pinSW) == LOW) {
+    delay(50);  // debounce
+    if (digitalRead(pinSW) == LOW) {
+      Consumer.write(MEDIA_VOLUME_MUTE);
+      delay(300);  // biar ga spam
+    }
+  }
+}
